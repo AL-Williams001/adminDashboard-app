a1f44a6d1fdf1a82671b1409927d87312854c14c
AL-Williams001 <arthurlynnwilliams01@gmail.com>
2024-05-13 14:10:29 +0800

    add monthly page

--- client/src/App.js
+++ client/src/App.js
@@ -12,6 +12,7 @@ import Transaction from "scenes/Transactions";
 import Geography from "scenes/Geography";
 import Overview from "scenes/Overview";
 import Daily from "scenes/Daily";
+import Monthly from "scenes/Monthly";
 const App = () => {
   const mode = useSelector((state) => state.global.mode);
   const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
@@ -31,6 +32,7 @@ const App = () => {
               <Route path="/geography" element={<Geography />} />
               <Route path="/overview" element={<Overview />} />
               <Route path="/daily" element={<Daily />} />
+              <Route path="/monthly" element={<Monthly />} />
             </Route>
           </Routes>
         </ThemeProvider>
